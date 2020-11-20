12534ea36054083846858de4220ee2bb00ca0142
Claudio Bellani <c.bellani17@imperial.ac.uk>
2020-11-20 10:19:46 +0100

    Data from brown97 maniatis11 miles09 wagnermeter
     On branch main
     Your branch is up-to-date with 'origin/main'.
    
     Changes to be committed:
    	new file:   data/brown1997/appendix1.csv
    	new file:   data/maniatisetal2011/table4.csv
    	new file:   data/milesandsmith2009/table4.csv
    	new file:   data/tamangetal2019/table1.csv
    	deleted:    data/wagnermeters/cleaneddata.csv
    	new file:   data/wagnermeters/mydata.csv
    	modified:   declare_specific_gravity.py
    	renamed:    bakeretal2004.pdf -> literature/bakeretal2004.pdf
    	new file:   literature/lewisetal2013.pdf
    	new file:   literature/lewisetal2013_supplement.pdf
    	new file:   literature/maniatisetal2011.pdf
    
     Untracked files:
    	../.gitignore

--- carbonstock/declare_specific_gravity.py
+++ carbonstock/declare_specific_gravity.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
-# In[1]:
 import pandas as pd
+def fill_wsg(df1, path_to_wsg_df = './data'):
+    print(path_to_wsg_df)
+    df2 = pd.read_csv(path_to_wsg_df, header=None)
+    df2.columns = ['botanical name', 'specific gravity']
+    df = df1.merge(df2, how='left', on='botanical name')
+    if 'specific gravity_x' in df.columns:
+        df['specific gravity'] = [wsg_x if not pd.isnull(wsg_x) else wsg_y
+                                  for (wsg_x, wsg_y) in zip(df['specific gravity_x'], df['specific gravity_y'])
+                                 ]
+        df.drop(columns=['specific gravity_x', 'specific gravity_y'], inplace=True)
+    return df
 path_data = "./data/"
 df = pd.read_csv(path_data+"measures.csv")
 df['botanical name'] = [name[name.find('-')+2:] for name in df['Specie']]
@@ -9,8 +19,13 @@ df['common name'] = [name[:name.find('-')-1] for name in df['Specie']]
 df.columns = ['name', 'seeds', 'trees', 'use', 'diameter', 'height', 'density', 'AGB',
        'source', 'botanical name', 'common name']
 df.drop(columns='density', inplace=True)
-wm = pd.read_csv(path_data+"wagnermeters/cleaneddata.csv", header=None)
-wm.columns = ['common name', 'botanical name', 'specific gravity', 'verified']
-wm.drop(columns='verified', inplace=True)
-df = df.merge(wm, on='botanical name', how='inner', suffixes=[' original',' wagnermeters'])
-df.to_csv(path_data+'measures_wagnermeters.csv')
+df.sort_values(by='botanical name', inplace=True, ignore_index=True)
+paths_sources = [
+    path_data+"wagnermeters/mydata.csv",
+    path_data+"maniatisetal2011/table4.csv",
+    path_data+"tamangetal2019/table1.csv",
+    path_data+'milesandsmith2009/table4.csv',
+]
+for source in paths_sources:
+    df = fill_wsg(df,source)
+df.to_csv(path_data+'ourforest.csv')
